fbb8e3754f2d388adeb531b8e9ffcdd4d19da2aa
Ramazan <ramazan.halid.35@gmail.com>
2021-06-27 00:19:12 +0300

    User entity updated

--- ToDoList/Entities/User.cs
+++ ToDoList/Entities/User.cs
@@ -2,8 +2,13 @@ namespace ToDoList.Entities
 {
     public class User:IEntity
     {
-        public int UserId { get; set; }
-        public string UserName { get; set; }
-        public string Password { get; set; }
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PasswordHash { get; set; }
+        public string PasswordSalt { get; set; }
+        public bool Status { get; set; }
+        
     }
 }
