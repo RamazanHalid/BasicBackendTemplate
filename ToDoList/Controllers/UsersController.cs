971f309d1a9b5469e17cec3251f2af987c0dc1d7
Ramazan <ramazan.halid.35@gmail.com>
2021-06-26 23:30:02 +0300

    Users Controller created

---
+++ ToDoList/Controllers/UsersController.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.BusinessLogic;
+using ToDoList.Entities;
+
+namespace ToDoList.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private IUserService _userService;
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            var result = _userService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(User user)
+        {
+            var result = _userService.Add(user);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result);
+        }
+
+        
+        
+    }
+}
