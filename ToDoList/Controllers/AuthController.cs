5b93e43779e8bae8acd1f741a4144621eb701004
Ramazan <ramazan.halid.35@gmail.com>
2021-06-27 00:43:02 +0300

    AuthController created

---
+++ ToDoList/Controllers/AuthController.cs
@@ -0,0 +1,50 @@
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.BusinessLogic;
+using ToDoList.BusinessLogic.Abstract;
+using ToDoList.Entities;
+
+namespace ToDoList.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("login")]
+        public ActionResult Login(UserForLoginDto userForLoginDto)
+        {
+           
+            var userToLogin = _authService.Login(userForLoginDto);
+            if (userToLogin.Success)
+            {
+
+                return Ok(userToLogin);
+
+            }
+
+            return BadRequest(userToLogin);
+            
+
+        }
+
+        [HttpPost("register")]
+        public ActionResult Register(UserForRegisterDto userForRegisterDto)
+        {
+            var userExists = _authService.UserExists(userForRegisterDto.Email);
+            if (userExists.Success)
+            {
+                _authService.Register(userForRegisterDto, userForRegisterDto.Password);
+                return Ok(userForRegisterDto);
+            }
+
+            return BadRequest(userExists.Message);
+  
+        }
+    }
+}
